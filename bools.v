85848709006d31781cfdc3dc6d9506423574239a
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2022-02-06 21:35:37 +1100

    exercise 1

---
+++ bools.v
@@ -0,0 +1,82 @@
+From Coq Require Export String.
+
+Inductive bool : Type :=
+  | true
+  | false.
+
+Definition negb (b:bool) : bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+
+Definition andb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => b2
+  | false => false
+  end.
+
+Definition orb  (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => true
+  | false => b2
+  end.
+
+Example test_orb1: (orb true false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_orb2: (orb false false) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_orb3: (orb false true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_orb4: (orb true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+(* Custom notation *)
+
+Notation "x && y" := (andb x y).
+Notation "x || y" := (orb x y).
+
+Example test_orb5: false || false || true = true.
+Proof. simpl. reflexivity. Qed.
+
+(* Exercise 1 *)
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  negb (andb b1 b2).
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool
+  := andb (andb b1 b2) b3
+
+Example test_andb31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- weekdays.v
+++ weekdays.v
@@ -6,3 +6,24 @@ Inductive day : Type :=
   | friday
   | saturday
   | sunday.
+
+Definition next_weekday (d:day) : day :=
+	match d with
+	| monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
+
+Compute (next_weekday friday).
+(* ==> monday : day *)
+Compute (next_weekday (next_weekday saturday)).
+(* ==> tuesday : day *)
+
+Example test_next_weekday:
+  (next_weekday (next_weekday saturday)) = tuesday.
+
+Proof. simpl. reflexivity. Qed.
