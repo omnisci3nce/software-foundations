7cf2b1faa99baafb3471e0ba9ef6b2dc903a8313
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2022-02-11 09:01:08 +1100

    numbers

--- numbers.v
+++ numbers.v
@@ -1,5 +1,35 @@
 Module NatPlayground.
 
+Inductive nat : Type :=
+  | O
+  | S (n : nat).
+
+Definition pred (n: nat) : nat :=
+  match n with
+  | O => O
+  | S n' => n'
+  end.
 
 
 End NatPlayground.
+
+Check (S (S (S (S O)))).
+
+Fixpoint even (n: nat) : bool :=
+  match n with
+  | O => true
+  | S O => false
+  | S (S n') => even n'
+  end.
+
+Definition odd (n:nat) : bool :=
+  negb (even n).
+
+Compute (even O).
+Compute (even (S O)).
+Compute (even (S (S O))).
+
+Example test_odd1: odd 1 = true.
+Proof. simpl. reflexivity. Qed.
+Example test_odd2: odd 4 = false.
+Proof. simpl. reflexivity. Qed.
